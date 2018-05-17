5faaf72f4ccb942a31175c4f065393a32369ab4e
Ju5t-a-R00k13 <34152364+Ju5t-a-R00k13@users.noreply.github.com>
2018-05-17 15:51:19 +0700

    Update SaltHash.js

--- js/SaltHash.js
+++ js/SaltHash.js
@@ -1,29 +1,18 @@
 'use strict';
 var crypto = require('crypto');
 
-/**
- * generates random string of characters i.e salt
- * @function
- * @param {number} length - Length of the random string.
- */
-var genRandomString = function(length){
+var CreateSalt = function(length){
     return crypto.randomBytes(Math.ceil(length/2))
             .toString('hex') /** convert to hexadecimal format */
             .slice(0,length);   /** return required number of characters */
 };
 
-/**
- * hash password with sha512.
- * @function
- * @param {string} password - List of required fields.
- * @param {string} salt - Data to be validated.
- */
-var sha512 = function(password, salt){
-    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-    hash.update(password);
+var sha512 = function(UsersPassword, Salt){
+    var hash = crypto.createHmac('sha512', Salt); /** Hashing algorithm sha512 */
+    hash.update(UsersPassword);
     var value = hash.digest('hex');
     return {
-        salt:salt,
+        salt:Salt,
         passwordHash:value
     };
 };
